232a5f879ddbd32d1a86e81e9c7375e8c95d5388
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-05-16 22:58:12 +0300

    Events are processed

--- vfusion_daemon/app/index.js
+++ vfusion_daemon/app/index.js
@@ -1,25 +1,34 @@
 const freeswitch = require('./init/freeswitch'),
     headersProcess = require('./init/fsheadersprocess');
 
-freeswitch.on('esl::event::CUSTOM::**', function(e) {
-	let headers = headersProcess(e.headers);
-    console.log("CUSTOM" + JSON.stringify(headers));
-
-})  .on('esl::event::CHANNEL_HANGUP::*', function(e) {
+freeswitch
+    .on('esl::event::CUSTOM::**', function(e) {
         let headers = headersProcess(e.headers);
-		console.log("CHANNEL_HANGUP " + JSON.stringify(headers));
-	})
+        console.log("CUSTOM" + JSON.stringify(headers));
+    })
+    .on('esl::event::CHANNEL_HANGUP::*', function(e) {
+        let headers = headersProcess(e.headers);
+        console.log("CHANNEL_HANGUP " + JSON.stringify(headers));
+    })
     .on('esl::event::CHANNEL_ORIGINATE::*', function(e) {
-		let headers = headersProcess(e.headers);
-		console.log("CHANNEL_ORIGINATE " + JSON.stringify(headers));
+        let headers = headersProcess(e.headers);
+        console.log("CHANNEL_ORIGINATE " + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_CREATE::*', function(e) {
-		let headers = headersProcess(e.headers);
-		console.log("CHANNEL_CREATE " + JSON.stringify(headers));
+        let headers = headersProcess(e.headers);
+        console.log("CHANNEL_CREATE " + JSON.stringify(headers));
+    })
+    .on('esl::event::CHANNEL_PROGRESS::*', function(e) {
+        let headers = headersProcess(e.headers);
+        console.log("CHANNEL_PROGRESS " + JSON.stringify(headers));
+    })
+    .on('esl::event::CHANNEL_BRIDGE::*', function(e) {
+        let headers = headersProcess(e.headers);
+        console.log("CHANNEL_BRIDGE " + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_ANSWER::*', function(e) {
-		let headers = headersProcess(e.headers);
-		console.log("CHANNEL_ANSWER " + JSON.stringify(headers));
-    });
+        let headers = headersProcess(e.headers);
+        console.log("CHANNEL_ANSWER " + JSON.stringify(headers));
+});
 
 console.log(new Date(), 'VFusion daemon started');
