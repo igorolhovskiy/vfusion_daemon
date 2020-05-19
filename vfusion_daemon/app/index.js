c19297f9caecb2374f6ff963165c738e855b5ca4
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-05-19 19:31:07 +0300

    ...

--- vfusion_daemon/app/index.js
+++ vfusion_daemon/app/index.js
@@ -6,19 +6,19 @@ const freeswitch = require('./init/freeswitch'),
 freeswitch
     .on('esl::event::CUSTOM::**', function(e) {
         let headers = headersProcess(e.headers);
-        log("CUSTOM" + JSON.stringify(headers));
+        //log("CUSTOM" + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_HANGUP::*', function(e) {
         let headers = headersProcess(e.headers);
-        log("CHANNEL_HANGUP " + JSON.stringify(headers));
+        //log("CHANNEL_HANGUP " + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_ORIGINATE::*', function(e) {
         let headers = headersProcess(e.headers);
-        log("CHANNEL_ORIGINATE " + JSON.stringify(headers));
+        //log("CHANNEL_ORIGINATE " + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_CREATE::*', function(e) {
         let headers = headersProcess(e.headers);
-        log("CHANNEL_CREATE " + JSON.stringify(headers));
+        //log("CHANNEL_CREATE " + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_PROGRESS::*', function(e) {
         let headers = headersProcess(e.headers);
@@ -26,11 +26,11 @@ freeswitch
     })
     .on('esl::event::CHANNEL_BRIDGE::*', function(e) {
         let headers = headersProcess(e.headers);
-        log("CHANNEL_BRIDGE " + JSON.stringify(headers));
+        //log("CHANNEL_BRIDGE " + JSON.stringify(headers));
     })
     .on('esl::event::CHANNEL_ANSWER::*', function(e) {
         let headers = headersProcess(e.headers);
-        log("CHANNEL_ANSWER " + JSON.stringify(headers, null, 2));
+        //log("CHANNEL_ANSWER " + JSON.stringify(headers, null, 2));
 });
 
 log('VFusion daemon started');
